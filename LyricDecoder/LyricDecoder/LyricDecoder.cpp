27936e93e26ef07e542a9b210901503e49d7454e
wangqr <wangqr@wangqr.tk>
2019-10-05 09:22:50 -0400

    Remove external dependency on QQMusicCommon.dll
    DES implementation copied from https://github.com/wangqr/QQMusicDES

--- LyricDecoder/LyricDecoder/LyricDecoder.cpp
+++ LyricDecoder/LyricDecoder/LyricDecoder.cpp
@@ -2,25 +2,17 @@
 #include "zlib/zconf.h"
 #include "zlib/zlib.h"
 #include "stdio.h"
+#include "des.h"
 #include <sys/stat.h>
 #include <malloc.h>
 #include <stdlib.h>
 
 #include<string.h>
-#include<windows.h>
 
-const char key[] = {64, 71, 97, 119, 94, 50, 116, 71, 81, 54, 49, 45, 206, 210,110, 105};
+const unsigned char key[] = { 64, 71, 97, 119, 94, 50, 116, 71, 81, 54, 49, 45, 206, 210, 110, 105 };
 
-//unsigned char QQKey1[] = "!@#)(NHLiuy*$%^&";
-//unsigned char QQKey2[] = "123ZXC!@#)(*$%^&";
-//unsigned char QQKey3[] = "!@#)(*$%^&abcDEF";
-unsigned char QQKey1[] = { 0x21, 0x40, 0x23, 0x29, 0x28, 0x4E, 0x48, 0x4C, 0x69, 0x75, 0x79, 0x2A, 0x24, 0x25, 0x5E, 0x26 };
-unsigned char QQKey2[] = { 0x31, 0x32, 0x33, 0x5A, 0x58, 0x43, 0x21, 0x40, 0x23, 0x29, 0x28, 0x2A, 0x24, 0x25, 0x5E, 0x26 };
-unsigned char QQKey3[] = { 0x21, 0x40, 0x23, 0x29, 0x28, 0x2A, 0x24, 0x25, 0x5E, 0x26, 0x61, 0x62, 0x63, 0x44, 0x45, 0x46 };
-typedef int(*QQdes)(unsigned char *, unsigned char *, int);
-typedef int(*QQDdes)(unsigned char *, unsigned char *, int);
-typedef int(*QQUncompressCommon)(unsigned char *, unsigned long *, unsigned char const *, unsigned long);
-
+//unsigned char QQKey[] = "!@#)(*$%123ZXC!@!@#)(NHL";
+const unsigned char QQKey[] = { 0x21, 0x40, 0x23, 0x29, 0x28, 0x2A, 0x24, 0x25, 0x31, 0x32, 0x33, 0x5A, 0x58, 0x43, 0x21, 0x40, 0x21, 0x40, 0x23, 0x29, 0x28, 0x4E, 0x48, 0x4C };
 
 
 char *krcdecode(char *src,int src_len){
@@ -51,12 +43,7 @@ char *krcdecode(char *src,int src_len){
 char *qrcdecode(char *src, int src_len) {
 
 	char qrc_hearder[] = "[offset:0]";
-	HMODULE hDLL = LoadLibrary(L"QQMusicCommon.dll");
-	if (!hDLL) {
-		return nullptr;
-	}
-	
-	
+
 	if (memcmp(src, qrc_hearder, 10) != 0) {
 		return nullptr;
 	}
@@ -65,12 +52,10 @@ char *qrcdecode(char *src, int src_len) {
 	unsigned long out_len = src_len * 16;
 	unsigned long *out_lenp = &out_len;
 
-	QQdes des = QQdes(GetProcAddress(hDLL, "?des@qqmusic@@YAHPAE0H@Z"));
-	QQDdes Ddes = QQDdes(GetProcAddress(hDLL, "?Ddes@qqmusic@@YAHPAE0H@Z"));
-	//QQUncompressCommon UncompressCommon = QQUncompressCommon(GetProcAddress(hDLL, "?UncompressCommon@qqmusic@@YAHPAEPAKPBEK@Z"));
-	Ddes((unsigned char *)(src + 11), QQKey1, src_len - 11);
-	des( (unsigned char *)(src + 11), QQKey2, src_len - 11);
-	Ddes((unsigned char *)(src + 11), QQKey3, src_len - 11);
+	BYTE schedule[3][16][6];
+	three_des_key_setup(QQKey, schedule, DES_DECRYPT);
+	for (int i = 11; i < src_len; i += 8)
+		three_des_crypt(reinterpret_cast<BYTE*>(src) + i, reinterpret_cast<BYTE*>(src) + i, schedule);
 
 	unsigned char *result = (unsigned char *)malloc(out_len);
 	
@@ -81,6 +66,5 @@ char *qrcdecode(char *src, int src_len) {
 		return nullptr;
 	}
 	result[out_len] = '\0';
-	FreeLibrary(hDLL);
 	return (char *)result;
 }
