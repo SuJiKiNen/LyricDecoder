917b5b0cbf8e7bd6b50b5ab69785a18aa8f236fc
wangqr <wangqr@wangqr.tk>
2019-10-16 19:18:39 -0400

    Use inflate API of zlib and handle insufficient output buffer

--- LyricDecoder/LyricDecoder/LyricDecoder.cpp
+++ LyricDecoder/LyricDecoder/LyricDecoder.cpp
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include<string.h>
 
@@ -15,56 +16,97 @@ const unsigned char key[] = { 64, 71, 97, 119, 94, 50, 116, 71, 81, 54, 49, 45, 
 const unsigned char QQKey[] = { 0x21, 0x40, 0x23, 0x29, 0x28, 0x2A, 0x24, 0x25, 0x31, 0x32, 0x33, 0x5A, 0x58, 0x43, 0x21, 0x40, 0x21, 0x40, 0x23, 0x29, 0x28, 0x4E, 0x48, 0x4C };
 
 
-char *krcdecode(char *src,int src_len){
+// Deflate from the src buffer. The returned memory is allocated using malloc, and the caller is responsible for freeing it.
+// A trailing '\0' is added to the decompress result
+// Returns NULL on failure.
+unsigned char* deflate_memory(Bytef* src, unsigned src_len) {
+	const size_t CHUNK = 262144;  // 256k chunk size as suggested by zlib doc
+	int ret = Z_STREAM_ERROR;
+	z_stream strm;
+	size_t dest_len = (src_len / CHUNK + 1) * CHUNK;  // up-rounding src_len to a multiple of CHUNK
+	Bytef* dest = (Bytef*)malloc(dest_len);
+	if (!dest) return NULL;
 
-	
-	if (!src) return nullptr;
-
-	if (memcmp(src, "krc1", 4) != 0) {
-		return nullptr;
+	// allocate inflate state
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	strm.next_in = src;
+	strm.avail_in = src_len;
+	strm.next_out = dest;
+	strm.avail_out = dest_len;
+	if (inflateInit(&strm) != Z_OK) {
+		free(dest);
+		return NULL;
 	}
-	
+	for (;;) {
+		if (!strm.avail_in)
+			break;
+		ret = inflate(&strm, Z_NO_FLUSH);
+		assert(ret != Z_STREAM_ERROR);  // state not clobbered
+		switch (ret) {
+		case Z_NEED_DICT:
+		case Z_DATA_ERROR:
+		case Z_MEM_ERROR:
+			inflateEnd(&strm);
+			free(dest);
+			return NULL;
+		}
+		if (strm.avail_out || ret == Z_STREAM_END)
+			break;
+		else {
+			Bytef* dest_new = (Bytef*)realloc(dest, dest_len + CHUNK);
+			if (dest_new) {
+				dest = dest_new;
+				strm.next_out = dest + dest_len;
+				strm.avail_out = CHUNK;
+				dest_len += CHUNK;
+			}
+			else {
+				inflateEnd(&strm);
+				free(dest);
+				return NULL;
+			}
+		}
+	}
+
+	// clean up and return
+	inflateEnd(&strm);
+	if (ret != Z_STREAM_END) {
+		free(dest);
+		return NULL;
+	}
+	dest_len -= strm.avail_out;
+	unsigned char* dest_new = (unsigned char*)realloc(dest, dest_len + 1);
+	if (dest_new)
+		dest = dest_new;
+	else {
+		free(dest);
+		return NULL;
+	}
+	dest[dest_len] = 0;
+	return dest;
+}
+
+char *krcdecode(char *src,int src_len){
+	if (src_len < 4 || memcmp(src, "krc1", 4) != 0)
+		return nullptr;
 
 	for (int i = 4; i < src_len; i++) {
 		src[i] = src[i] ^ key[(i-4) % 16];
 	}
-	uLongf dstsize = src_len * 100;
-	uLongf *dstsizep = &dstsize;
-	unsigned char *result = (unsigned char *)malloc(dstsize);
-	if (Z_OK != uncompress(result, dstsizep, (const Bytef*)(src + 4), src_len-4)) {
-		return nullptr;
-	}
-		
-	result[dstsize] = '\0';
 
-	return (char *)result;
+	return (char*)deflate_memory((Bytef*)src + 4, src_len - 4);
 }
 
 char *qrcdecode(char *src, int src_len) {
-
-	char qrc_hearder[] = "[offset:0]";
-
-	if (memcmp(src, qrc_hearder, 10) != 0) {
+	if (src_len < 10 || memcmp(src, "[offset:0]", 10) != 0)
 		return nullptr;
-	}
-	
-
-	unsigned long out_len = src_len * 16;
-	unsigned long *out_lenp = &out_len;
 
 	BYTE schedule[3][16][6];
 	three_des_key_setup(QQKey, schedule, DES_DECRYPT);
 	for (int i = 11; i < src_len; i += 8)
 		three_des_crypt(reinterpret_cast<BYTE*>(src) + i, reinterpret_cast<BYTE*>(src) + i, schedule);
 
-	unsigned char *result = (unsigned char *)malloc(out_len);
-	
-	//if (!result) return nullptr;
-	
-	//UncompressCommon(result, out_lenp, (unsigned char *)(src + 11), src_len - 11);
-	if (Z_OK != uncompress(result, out_lenp, (const Bytef*)(src + 11), src_len - 11)) {
-		return nullptr;
-	}
-	result[out_len] = '\0';
-	return (char *)result;
+	return (char*)deflate_memory((Bytef*)src + 11, src_len - 11);
 }
